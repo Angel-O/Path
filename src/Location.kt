f4c56ef174803cc844f94173a4ff0133326986f9
Angelo <angelooparah@gmail.com>
2017-03-19 16:32:44 +0000

    tidying up, comments improvements

--- src/Location.kt
+++ src/Location.kt
@@ -1,5 +1,6 @@
+/* represents a puzzle's location */
 data class Location(val row:Int, val col:Int, val range:Int){
 
-	val coord = Pair(row, col)
-	val value = range
+	val coord = Pair(row, col) // coordinates
+	val value = range // value of the cell (move range)
 }
--- src/Puzzle.kt
+++ src/Puzzle.kt
@@ -3,31 +3,29 @@ import javax.swing.JFileChooser
 import java.util.Stack
 
 // Using JFileChooser and nullable type
-
+/* entry point */
 fun main(args: Array<String>) {
-//    val file = chooseFile()
-//    println(file ?: "No file selected")
-//
-//	// defensive programming
-//	if(file == null){
-//		return
-//	}
-	// solve the puzzle
-    //val matrix = createMatrix(file)
-	val puzzle = createMatrix(File("/Users/specs/git/Path/src/puzzle.txt"))
-	// debug
-	//puzzle.forEach({ println(it) })
+    val file = chooseFile()
+    println(file ?: "No file selected")
 
-	val stack = Stack<Location>()
-	stack.push(puzzle[0][0])
-	//println(stack[0].coord)
-	solvePuzzle(puzzle, stack, ArrayList<Location>())
+	// defensive programming
+	if(file == null){
+		return
+	}
 
-	//println(stack.contains(Location(0,0,7)))
-
-	//println("FIRST " + puzzle[0][0])
+	// creates a matrix containing each location
+	val puzzle = createMatrix(file) //File("/Users/specs/git/Path/src/puzzle.txt")
+	// create the stack that will contain the path that silves the puzzle
+	// and add the starting point (first top left corner of the puzzle)
+	val path = Stack<Location>()
+	path.push(puzzle[0][0])
+	// will collect dead ends that need not to be explored
+	val deadEnds = ArrayList<Location>()
+	// solve it
+	solvePuzzle(puzzle, path, deadEnds)
 }
 
+/* opens a pop up window to load the file containing the puzzle */
 fun chooseFile(): File? {
     val chooser = JFileChooser()
     val theFileResult = chooser.showOpenDialog(null)
@@ -40,18 +38,14 @@ fun chooseFile(): File? {
     return null
 }
 
+/* creates the matrix representing the puzzle to be solved */
 fun createMatrix(file: File) : ArrayList<List<Location>>{
 	val matrix = ArrayList<List<Location>>()
-
 	// initial coordinates
 	var row: Int = 0; var col: Int = 0
-
+	// split each line parsing each chunk to an int and create a Location data class
 	for (line in file.readLines()){
-
-		// split each line parsing each chunk to an int
-		// and create a Location object adding the coordinates of the cell
 		matrix.add(line.split(" ").map{ Location(row, col++, it.toInt()) })
-
 		// at each iteration increment the row and reset the column count
 		row++; col = 0
 	}
@@ -63,17 +57,18 @@ fun solvePuzzle(puzzle: ArrayList<List<Location>>, path: Stack<Location>, fullyE
 
 	when{
 		// if we backtracked all the way back to the initial state, quit
-		path.isEmpty() -> { println("the puzzle cannot be solved") }
-		// it's solved display the path
-		solved(puzzle, path) -> { println(path.map( {it.coord} )) }
-		// otherwise solve the puzzle
+		path.isEmpty() -> { println("The puzzle cannot be solved.") }
+		// if the puzzle is solved display the solution
+		solved(puzzle, path) -> { println("The solution to the puzzle is:\n" + path.map( {it.coord} )) }
+		// otherwise solve it
 		else -> {
-				// get all possible destinations from the current cell (on top of the path stack)
-				val destinations = getDestinations(puzzle, path.peek(), fullyExplored) //destinations.forEach({println(it.range)})
+				// get all possible destinations from the current cell (the one on top of the path stack)
+				val destinations = getDestinations(puzzle, path.peek(), fullyExplored)
 				when{
 					// if there is nowhere to go from the current location, backtrack...
 					destinations.isEmpty() -> {
-						fullyExplored.add(path.pop()); // mark the current location as fully explored since we can't go anywhere else
+						// mark the current location (top of the stack) as fully explored since we can't go anywhere else
+						fullyExplored.add(path.pop());
 						return solvePuzzle(puzzle, path, fullyExplored)
 					}
 					else -> { // otherwise keep exploring
@@ -82,15 +77,15 @@ fun solvePuzzle(puzzle: ArrayList<List<Location>>, path: Stack<Location>, fullyE
 						while(!destinations.isEmpty()){
 							val nextRoute = destinations.pop()
 							if (!path.toList().contains(nextRoute)){
-								// if the location hasn't been explored yet, explore it
+								// if the location hasn't been explored yet, add it to the path and proceed exploring
 								path.push(nextRoute)
 								return solvePuzzle(puzzle, path, fullyExplored)
 							}
 						}
-						// if none of the destinations turned out to be succesfull backtrack...
-						// NOTE: quite curiously if we never return the value of the function
-						// this block of code will add to the fully explored list while the
-						// puzzled is being explored above!!! concurrency for free!!!
+						// if none of the destinations turned out to be successful backtrack...
+						// (NOTE: quite curiously if we never return the value of the function
+						// whenever it's called this block of code will add to the fully explored list
+						// while the puzzled is being explored above!!! concurrency for free!!!)
 						if(!path.isEmpty()){
 							val explored = path.pop()
 							fullyExplored.add(explored)
@@ -102,45 +97,51 @@ fun solvePuzzle(puzzle: ArrayList<List<Location>>, path: Stack<Location>, fullyE
 		}
 }
 
-/* flags whether or not the bottom right corner of the puzzle was reached */
+/* checks whether or not the bottom right corner of the puzzle has been reached and therefore the puzzle solved */
 fun solved(matrix: ArrayList<List<Location>>, stack: Stack<Location>): Boolean{
 	return !stack.isEmpty() && stack.peek().coord == Pair(matrix.size - 1, matrix.size - 1)
 }
 
-/* get a stack of all possible destinations from the current cell based on the range of movement from
+/* gets a stack of all possible destinations from the current cell based on the range of movement from
   the cell itself */
 fun getDestinations(matrix: ArrayList<List<Location>>, currentLocation: Location, fullyExplored: ArrayList<Location>): Stack<Location>{
 	val destinations = Stack<Location>()
 	val row = currentLocation.coord.first
 	val col = currentLocation.coord.second
 	val range = currentLocation.range;
-	//val goalCoord = matrix.size - 1; // minus 1 ????
 	destinations.addAll(listOf(
 			createLocation(row, col + range, matrix), // right
 			createLocation(row, col - range, matrix), // left
 			createLocation(row - range, col, matrix), // top
 			createLocation(row + range, col, matrix)) // bottom
-			.filter({it != null // ignore out of range locations
-					//&& (it.range != 0 && it.coord != Pair(goalCoord, goalCoord)) // get rid of invalid destinations
-					&& it.range < matrix.size // ignore invalid locations that would certainly lead outside the matrix
+			.filter({it != null // ignore null locations (the ones that fall out of the puzzle's boudaries)
+					&& (it.range != 0) // ignore destinations that would get us stuck
+					&& it.range < matrix.size // ignore locations that would certainly lead to invalid locations outside the matrix
 					&& !fullyExplored.contains(it)})) // ignore already visited locations leading to nowhere
 
 	return destinations
 }
 
-/* return a valid coordinate if in range otherwise a null value */
+/* returns a valid coordinate if in range otherwise a null value */
 fun createLocation(row: Int, col: Int, matrix: ArrayList<List<Location>>): Location?{
 	val puzzleSize = matrix.size
 	var location: Location?
 	when{
+		// create a location data class if the coordinates are in range
 		inRange(row, puzzleSize)
-		&& inRange(col, puzzleSize) -> { location = Location(row, col, matrix[row][col].range) }
+		&& inRange(col, puzzleSize) -> {
+			location = Location(
+					row, // valid row coordinate
+					col, // valid column coordinate
+					// if it's the goal cell create a location data class with a valid range so that it won't be seen as an invalid location
+					if (row == puzzleSize - 1 && col == puzzleSize -1) 1 else matrix[row][col].range)
+		}
 		else -> location = null
 	}
 	return location
 }
 
-/* check if the coordinate is in range */
+/* checks if the coordinate is withing the puzzle borders */
 fun inRange(coord: Int, size: Int): Boolean{
 	return coord >= 0 && coord < size
 }
