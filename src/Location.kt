f06b97adf265fa11d0dfe0e598b024ee0d6fbb05
Angelo <angelooparah@gmail.com>
2017-03-19 09:17:21 +0000

    shaping up the algorithm

--- src/Location.kt
+++ src/Location.kt
@@ -1,4 +1,4 @@
-class Location(val row:Int, val col:Int, val range:Int){
+data class Location(val row:Int, val col:Int, val range:Int){
 
 	val coord = Pair(row, col)
 	val value = range
--- src/Puzzle.kt
+++ src/Puzzle.kt
@@ -20,8 +20,10 @@ fun main(args: Array<String>) {
 
 	val stack = Stack<Location>()
 	stack.push(puzzle[0][0])
-	println(stack[0].coord)
-	solve(puzzle, stack)
+	//println(stack[0].coord)
+	explore(puzzle, stack)
+
+	//println(stack.contains(Location(0,0,7)))
 
 	//println("FIRST " + puzzle[0][0])
 }
@@ -56,45 +58,82 @@ fun createMatrix(file: File) : ArrayList<List<Location>>{
 	return matrix
 }
 
-fun solve(matrix: ArrayList<List<Location>>, path: Stack<Location>){
+fun explore(matrix: ArrayList<List<Location>>, path: Stack<Location>){
 
-	// get the range
-	val move = path.peek().range
+	//if (path.isEmpty()) {path.push(matrix[0][0]); println("path was empty");} // backtracked too much...let's restart : SHOULD NOT HAPPEN
+	when{
+		solved(matrix, path) -> { path.forEach({println(it.coord)}); return} // print the path...
+		else -> {
 
-	// get the possible destinations reacheable from the location on top of the stack
-	val destinations = getDestinations(matrix, path.peek())
+				// get all possible destinations from the current cell (on top of the path stack)
+				val destinations = getDestinations(matrix, path.peek())
 
-	destinations.forEach({println(it.range)})
+				//destinations.forEach({println(it.range)})
+				// explore all destinations...
+				while(!destinations.isEmpty()){
+
+					val nextRoute = destinations.pop()
+					if (!path.toList().contains(nextRoute)){
+						// if the location hasn't been explored yet, explore it
+						path.push(nextRoute)
+						//println(path.map{it.coord})
+						explore(matrix, path)
+					}
+//					else{
+//						// otherwise remove it...
+//						//path.pop();
+//						//explore(matrix, path)
+//
+//						println(nextRoute)
+//					}
+				}
+				// if the destinations stack is empty we have visited all the location
+				// but did not found a good route
+				//path.pop();
+				//if (path.isEmpty()) path.push(matrix[0][0])
+				//explore(matrix, path)
+				// if none of the destinations turned out to be succesfull backtrack...
+				//while(path.peek() != currentLocation){
+//			if(!path.isEmpty()){
+//				path.pop()
+//				explore(matrix, path)
+//			}
+			 // what if I get back to the beginning??
+				//}
+			//path.forEach({println(it.coord)})
+
+			}
+
+		}
+}
+
+fun solved(matrix: ArrayList<List<Location>>, stack: Stack<Location>): Boolean{
+	return stack.peek().coord == Pair(matrix.count() - 1, matrix.count() - 1)
 }
 
 /* get a stack of all possible destinations from the current cell based on the range of movement from
   the cell itself */
 fun getDestinations(matrix: ArrayList<List<Location>>, currentLocation: Location): Stack<Location>{
-
 	val destinations = Stack<Location>()
-
 	val row = currentLocation.coord.first
 	val col = currentLocation.coord.second
 	val range = currentLocation.range;
-
-	val right = getLocationIfInRange(row, col + range, matrix)
-	val left = getLocationIfInRange(row, col - range, matrix)
-	val top = getLocationIfInRange(row - range, col, matrix)
-	val bottom = getLocationIfInRange(row + range, col, matrix)
-
-	// ignore null entries
-	destinations.addAll(listOf(right, left, top , bottom).filter({it != null}))
-
+	destinations.addAll(listOf(
+			createLocation(row, col + range, matrix), // right
+			createLocation(row, col - range, matrix), // left
+			createLocation(row - range, col, matrix), // top
+			createLocation(row + range, col, matrix)) // bottom
+			.filter({it != null && it.range != 0 && it.range < matrix.count()})) // get rid of invalid destinations
 	return destinations
 }
 
 /* return a valid coordinate if in range otherwise a null value */
-fun getLocationIfInRange(row: Int, col: Int, matrix: ArrayList<List<Location>>): Location?{
-
+fun createLocation(row: Int, col: Int, matrix: ArrayList<List<Location>>): Location?{
 	val puzzleSize = matrix.count()
 	var location: Location?
 	when{
-		inRange(row, puzzleSize) && inRange(col, puzzleSize) -> location = Location(row, col, matrix[row][col].range)
+		inRange(row, puzzleSize)
+		&& inRange(col, puzzleSize) -> location = Location(row, col, matrix[row][col].range)
 		else -> location = null
 	}
 	return location
@@ -102,7 +141,6 @@ fun getLocationIfInRange(row: Int, col: Int, matrix: ArrayList<List<Location>>):
 
 /* check if the coordinate is in range */
 fun inRange(coord: Int, size: Int): Boolean{
-
 	return coord >= 0 && coord < size
 }
 
